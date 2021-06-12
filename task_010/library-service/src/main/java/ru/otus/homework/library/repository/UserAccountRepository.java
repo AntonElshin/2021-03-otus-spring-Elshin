585b269252fa717c9ce08a2e1809ab3b2b19a55a
Anton Elshin <anton.elshin@mail.ru>
2021-06-12 23:47:59 +0300

    task010 - В CRUD Web-приложение добавить механизм аутентификации

--- task_010/library-service/src/main/java/ru/otus/homework/library/repository/UserAccountRepository.java
+++ task_010/library-service/src/main/java/ru/otus/homework/library/repository/UserAccountRepository.java
@@ -1,11 +1,11 @@
 package ru.otus.homework.library.repository;
 
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.homework.library.domain.UserAccount;
 
 import java.util.Optional;
 
-public interface UserAccountRepository extends PagingAndSortingRepository<UserAccount, Long> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
 
     Optional<UserAccount> findByLoginEqualsAndStatusEquals(String login, String status);
 }
